158586cb6813dd398cdb767de5ca0f229dbe5270
Amanda Jopp <aclaire488@gmail.com>
2017-01-04 11:18:27 -0600

    basics done

--- scripts/ngLaunchpad.js
+++ scripts/ngLaunchpad.js
@@ -2,6 +2,15 @@ console.log('js');
 
 var myApp = angular.module('myApp', []);
 
+
+
 myApp.controller('LaunchpadController', ['$scope', function($scope){
   console.log('NG');
+  $scope.favoriteMovies = [];
+  $scope.getMovieName = function(){
+    console.log( 'in getMovieName()');
+    console.log( 'movieName:', $scope.movieName );
+    $scope.favoriteMovies.push( $scope.movieName );
+    console.log( 'favoriteMovies:', $scope.favoriteMovies );
+  }; // end getMovieName
 }]); //end controller
